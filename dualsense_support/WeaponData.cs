342c6d119e47c4d4381c08bc5ea38590e03bbfa8
Aleksandr Shishkov <aleksandr.shishkov@student.um.si>
2023-03-12 18:15:13 +0100

    Remove unused things

--- dualsense_support/WeaponData.cs
+++ dualsense_support/WeaponData.cs
@@ -14,14 +14,15 @@ public class WeaponData
         Name = name;
     }
 
+    public WeaponData(int id, string name, string instructions)
+    {
+        Id = id;
+        Name = name;
+        Instructions = instructions;
+    }
+
     public int Id { get; set; }
     public string Name { get; set; }
 
     public string? Instructions { get; set; }
-}
-
-public class MyInstruction
-{
-    public int Type { get; set; }
-    public int[] Parameters { get; set; }
 }
